bc16a6c5de25889298af79d2797172c02dce3d9c
Adam Emieljaniuk <beginner1986@gmail.com>
2020-01-23 18:49:28 +0100

    Block class template added.

---
+++ Blockchain/Block.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blockchain
+{
+    class Block
+    {
+        int blockNumber;
+        byte[] merkleHash;
+        byte[] prevHash;
+        DateTime mineTime;
+        long randomNumber;
+        byte[] thisHash;
+        int j;
+
+        Block(List<byte[]> payments, byte[] prevHash, int j)
+        {
+
+        }
+    }
+}
