47f67fe628d81af2fb8c7f437ba026fdeabad945
Adam Emieljaniuk <beginner1986@gmail.com>
2020-01-23 20:42:10 +0100

    Full block hash counting implemented.

--- Blockchain/Block.cs
+++ Blockchain/Block.cs
@@ -7,17 +7,27 @@ namespace Blockchain
 {
     class Block
     {
-        private int blockNumber;
-        private byte[] merkleHash;
-        private byte[] prevHash;
-        private DateTime mineTime;
+        static int BlocksCount = 0;
+
+        private readonly byte[] merkleHash;
+        private readonly byte[] prevHash;
+        private readonly DateTime mineTime;
+        private readonly int blockNumber;
         private long randomNumber;
         private byte[] thisHash;
-        private int j;
 
         public Block(List<byte[]> payments, byte[] prevHash, int j)
         {
-            // TODO
+            Random random = new Random();
+
+            blockNumber = BlocksCount++;
+            merkleHash = MerkleTree(payments);
+            this.prevHash = prevHash;
+            mineTime = DateTime.Now;
+            randomNumber = random.Next();
+
+            byte[] fullBlock = MakeFullBlock(merkleHash, prevHash, mineTime, blockNumber, randomNumber);
+
 
             /*
             // Merkle tree test
@@ -65,6 +75,47 @@ namespace Blockchain
             }
 
             return hashes[0];
+        }
+
+        private int CountZeros(byte[] hash)
+        {
+            int result = 0;
+            for(int i=0; i<hash.Length; i++)
+            {
+                if (hash[i] == 0)
+                    result++;
+                else
+                    break;
+            }
+
+            return result;
+        }
+
+        private byte[] MakeFullBlock(byte[] merkleHash, byte[] prevHash,
+        DateTime mineTime, int blockNumber, long randomNumber)
+        {
+            byte[] mineTimeBytes = Encoding.UTF8.GetBytes(mineTime.ToString());
+            byte[] blockNumberBytes = BitConverter.GetBytes(blockNumber);
+            byte[] randomNumberBytes = BitConverter.GetBytes(randomNumber);
+            byte[] fullBlock =
+                new byte[merkleHash.Length + prevHash.Length + mineTimeBytes.Length
+                + blockNumberBytes.Length + randomNumberBytes.Length];
+
+            byte[] result = new byte[merkleHash.Length + prevHash.Length + mineTimeBytes.Length
+                + blockNumberBytes.Length + randomNumberBytes.Length];
+
+            int position = 0;
+            Array.Copy(merkleHash, 0, result, position, merkleHash.Length);
+            position += merkleHash.Length;
+            Array.Copy(prevHash, 0, result, position, prevHash.Length);
+            position += prevHash.Length;
+            Array.Copy(mineTimeBytes, 0, result, position, mineTimeBytes.Length);
+            position += mineTimeBytes.Length;
+            Array.Copy(blockNumberBytes, 0, result, position, blockNumberBytes.Length);
+            position += blockNumberBytes.Length;
+            Array.Copy(randomNumberBytes, 0, result, position, randomNumberBytes.Length);
+
+            return result;
         }
     }
 }
--- Blockchain/Program.cs
+++ Blockchain/Program.cs
@@ -8,15 +8,17 @@ namespace Blockchain
     {
         static void Main(string[] args)
         {
+            // input payments
             SHA256 sha256 = SHA256.Create();
             byte[] prevHash = sha256.ComputeHash(new byte[] { 9 });
             List<byte[]> payments = new List<byte[]>();
+            // number of expected 0s in the beginning
             int j = 5;
 
             for (byte i = 0; i < 7; i++)
                 payments.Add(sha256.ComputeHash(new byte[] { i }));
 
-            Block block = new Block(payments, prevHash, 3);
+            Block block = new Block(payments, prevHash, j);
         }
     }
 }
